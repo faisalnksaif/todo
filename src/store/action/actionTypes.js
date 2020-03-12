79fe3af2b3691db7bed04cf308cdb4cfe32ed865
Faisal Saif <faisal@qburst.com>
2020-03-12 14:39:24 +0530

    removing store

--- src/store/action/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const SIGNUP = "SIGNUP";
-export const SIGNUP_ERROR = "SIGNUP_ERROR";
-export const CLEAR_SIGNUP_ERROR = "CLEAR_SIGNUP_ERROR";
-export const LOADING = "LOADING";
-
-export const STORE_TOKEN = "STORE_TOKEN";
-export const LOGIN_FAILED = "LOGIN_FAILED";
-export const GET_TOKEN = "GET_TOKEN";
